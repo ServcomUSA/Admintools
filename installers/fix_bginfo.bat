c2cb1bc064d8da0716d4b909eca9494040c40577
servcomusa-wrogers <30842844+servcomusa-wrogers@users.noreply.github.com>
2018-05-08 18:10:37 -0400

    Ticket #61586
    Permanent fixes for bginfo compatibility on x86 and x64 machines.

--- installers/fix_bginfo.bat
+++ installers/fix_bginfo.bat
@@ -1,10 +1,22 @@
 @echo off
 
 set taskname="Update Wallpaper Info"
+set OS=""
+
+
 cd \
 schtasks /query | find %taskname% > NUL
 IF ERRORLEVEL 1 goto :quit
-schtasks /change /tn %taskname% /tr "c:\admintools\bginfo64.exe c:\admintools\bginfo.bgi /SILENT /NOLICPROMPT /TIMER:00"
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+if %OS%==32BIT (
+	schtasks /change /tn %taskname% /tr "c:\admintools\bginfo.exe c:\admintools\bginfo.bgi /SILENT /NOLICPROMPT /TIMER:00"
+)
+if %OS%==64BIT (
+	schtasks /change /tn %taskname% /tr "c:\admintools\bginfo64.exe c:\admintools\bginfo.bgi /SILENT /NOLICPROMPT /TIMER:00"
+)
+
 IF ERRORLEVEL 1 goto :fail
 echo Task updated.
 goto :eof
--- installers/set_bginfo.bat
+++ installers/set_bginfo.bat
@@ -5,6 +5,7 @@ setlocal ENABLEEXTENSIONS
 set REG_NAME="HKLM\Software\Wow6432Node\Servcom USA\Admintools"
 set KEY_NAME="bginfo_path"
 set bginfo_path=""
+set OS=""
 
 FOR /F "usebackq skip=2 tokens=1-3" %%A IN (`REG QUERY %REG_NAME% /v %KEY_NAME% 2^>nul`) DO SET bginfo_path=%%C
 
@@ -12,7 +13,14 @@ if "%bginfo_path%"=="" (
 	ECHO "no path; quit"
 	GOTO :EOF
 )
-schtasks /create /tn "Update Wallpaper Info" /tr "%bginfo_path%\bginfo64.exe %bginfo_path%\bginfo.bgi /silent /nolicprompt /timer:0" /sc ONLOGON /F  
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==32BIT (
+	schtasks /create /tn "Update Wallpaper Info" /tr "%bginfo_path%\bginfo.exe %bginfo_path%\bginfo.bgi /silent /nolicprompt /timer:0" /sc ONLOGON /F  
+)
+if %OS%==64BIT (
+	schtasks /create /tn "Update Wallpaper Info" /tr "%bginfo_path%\bginfo64.exe %bginfo_path%\bginfo.bgi /silent /nolicprompt /timer:0" /sc ONLOGON /F  	
+)
 schtasks /run /tn "Update Wallpaper Info" /I
 
 endlocal
